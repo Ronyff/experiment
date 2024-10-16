40e54123b41471e0ff811e4c1b55b8abca75ece8
Md_Rony <ronyff534@gmail.com>
2024-10-16 20:41:30 +0600

    first commit on development branch

--- operation.cpp
+++ operation.cpp
@@ -1,10 +1,16 @@
 #include<iostream>
 using namespace std;
+double add(double x, double Y){
+    return x + Y;
+}
  
 int main(){
     double a,b;
     cout << "Enter two value" <<endl;
     cin >> a >> b;
+
+   double addition =  add(a,b);
+   cout << addition << endl;
 
 
 
